ae4aac2f310e50fe962698fb43608501489fc515
曹鈞翔 <caojunxiang@caojunxiangdeMacBook-Pro.local>
2022-05-24 22:44:11 +0800

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello world";
+	}
+	@GetMapping("/list-hello")
+	public List<String> hello_list(){
+		// return with a json List format.
+		return List.of("Hi", "There");
+	}
+
+}

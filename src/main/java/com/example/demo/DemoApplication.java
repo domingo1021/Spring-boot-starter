6d116f89b0116c53593e60fb42054fe191c8aa0b
曹鈞翔 <caojunxiang@caojunxiangdeMacBook-Pro.local>
2022-05-24 23:34:46 +0800

    Modify list-hello for student json

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -20,9 +23,17 @@ public class DemoApplication {
 		return "Hello world";
 	}
 	@GetMapping("/list-hello")
-	public List<String> hello_list(){
+	public List<Student> hello_list(){
 		// return with a json List format.
-		return List.of("Hi", "There");
+		return List.of(
+				new Student(
+						107306075L,
+						"Tsao",
+						"domingo0204@gmail.com",
+						LocalDate.of(2000, Month.OCTOBER, 21),
+						21
+				)
+		);
 	}
 
 }

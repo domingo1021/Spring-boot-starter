83b230b660e1d4f832b76670b5bcd9e8cddb1b1d
曹鈞翔 <caojunxiang@caojunxiangdeMacBook-Pro.local>
2022-05-25 15:47:27 +0800

    add Services and Controller

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,2 +1,23 @@
-package com.example.demo.student;public class StudentService {
+package com.example.demo.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents(){
+        // return with a json List format.
+        return List.of(
+                new Student(
+                        107306075L,
+                        "Tsao",
+                        "domingo0204@gmail.com",
+                        LocalDate.of(2000, Month.OCTOBER, 21),
+                        21
+                )
+        );
+    }
 }
